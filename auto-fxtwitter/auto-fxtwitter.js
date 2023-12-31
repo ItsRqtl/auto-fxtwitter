6897cf6dd9c63a99247668cd41f76ae08d5c2fdf
ItsRqtl <itsrqtl@gmail.com>
2023-12-30 19:04:06 -0800

    fix: add preventDefault to ensure the clipboard get the converted url

--- auto-fxtwitter/auto-fxtwitter.js
+++ auto-fxtwitter/auto-fxtwitter.js
@@ -1,4 +1,4 @@
-function modifyClipboard() {
+function modifyClipboard(event) {
     const clipboardText = window.getSelection().toString()
     if (clipboardText.includes("x.com") || clipboardText.includes("twitter.com")) {
         const modifiedText = clipboardText
@@ -8,6 +8,7 @@ function modifyClipboard() {
         navigator.clipboard.writeText(modifiedText)
             .then(() => {console.log("Successfully modified clipboard: " + modifiedText)})
             .catch((error) => {console.error("Failed to modify clipboard: " + error)});
+        event.preventDefault();
     }
 }
 
