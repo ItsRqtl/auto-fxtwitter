b56195f3eed9b6d3f5fc97ccf888d4bc28ac641a
ItsRqtl <itsrqtl@gmail.com>
2023-12-30 23:04:33 -0800

    style: format code with prettier

--- auto-fxtwitter/auto-fxtwitter.js
+++ auto-fxtwitter/auto-fxtwitter.js
@@ -2,30 +2,37 @@ function modifyClipboard(event) {
     const clipboardText = window.getSelection().toString();
 
     if (clipboardText.includes("x.com") || clipboardText.includes("twitter.com")) {
-        chrome.storage.sync.get({
-            enabled: true,
-            replaceDomain: "fxtwitter.com",
-            removeParams: true
-        }, (preferences) => {
-            if (!preferences.enabled) {
-                return
+        chrome.storage.sync.get(
+            {
+                enabled: true,
+                replaceDomain: "fxtwitter.com",
+                removeParams: true,
+            },
+            (preferences) => {
+                if (!preferences.enabled) {
+                    return;
+                }
+
+                let modifiedText = clipboardText.replace(
+                    /(?:twitter|x)\.com/g,
+                    preferences.replaceDomain
+                );
+                if (preferences.removeParams) {
+                    modifiedText = modifiedText.replace(/\?.*$/, "");
+                }
+
+                navigator.clipboard
+                    .writeText(modifiedText)
+                    .then(() => {
+                        console.log("Successfully modified clipboard: " + modifiedText);
+                    })
+                    .catch((error) => {
+                        console.error("Failed to modify clipboard: " + error);
+                    });
+
+                event.preventDefault();
             }
-
-            let modifiedText = clipboardText.replace(/(?:twitter|x)\.com/g, preferences.replaceDomain);
-            if (preferences.removeParams) {
-                modifiedText = modifiedText.replace(/\?.*$/, '')
-            }
-
-            navigator.clipboard.writeText(modifiedText)
-                .then(() => {
-                    console.log("Successfully modified clipboard: " + modifiedText)
-                })
-                .catch((error) => {
-                    console.error("Failed to modify clipboard: " + error)
-                });
-
-            event.preventDefault();
-        })
+        );
     }
 }
 
--- auto-fxtwitter/popup.js
+++ auto-fxtwitter/popup.js
@@ -1,28 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const enabledCheckbox = document.getElementById('replaceCheckbox');
-    const replaceDomainInput = document.getElementById('replaceDomainInput');
-    const removeParamsCheckbox = document.getElementById('removeParamsCheckbox');
+document.addEventListener("DOMContentLoaded", () => {
+    const enabledCheckbox = document.getElementById("replaceCheckbox");
+    const replaceDomainInput = document.getElementById("replaceDomainInput");
+    const removeParamsCheckbox = document.getElementById("removeParamsCheckbox");
 
-    chrome.storage.sync.get({
-        enabled: true,
-        replaceDomain: "fxtwitter.com",
-        removeParams: true
-    }, (preferences) => {
-        enabledCheckbox.checked = preferences.enabled;
-        replaceDomainInput.value = preferences.replaceDomain;
-        removeParamsCheckbox.checked = preferences.removeParams;
-    });
+    chrome.storage.sync.get(
+        {
+            enabled: true,
+            replaceDomain: "fxtwitter.com",
+            removeParams: true,
+        },
+        (preferences) => {
+            enabledCheckbox.checked = preferences.enabled;
+            replaceDomainInput.value = preferences.replaceDomain;
+            removeParamsCheckbox.checked = preferences.removeParams;
+        }
+    );
 
-    enabledCheckbox.addEventListener('change', savePreferences);
-    replaceDomainInput.addEventListener('input', savePreferences);
-    removeParamsCheckbox.addEventListener('change', savePreferences);
+    enabledCheckbox.addEventListener("change", savePreferences);
+    replaceDomainInput.addEventListener("input", savePreferences);
+    removeParamsCheckbox.addEventListener("change", savePreferences);
 });
 
 function savePreferences() {
     const preferences = {
-        enabled: document.getElementById('replaceCheckbox').checked,
-        replaceDomain: document.getElementById('replaceDomainInput').value,
-        removeParams: document.getElementById('removeParamsCheckbox').checked
+        enabled: document.getElementById("replaceCheckbox").checked,
+        replaceDomain: document.getElementById("replaceDomainInput").value,
+        removeParams: document.getElementById("removeParamsCheckbox").checked,
     };
     chrome.storage.sync.set(preferences, () => {});
 }
